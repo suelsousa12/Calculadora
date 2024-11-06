5a05db4eb5a74abb2dcc686ae005443aa66d10a9
suelsousa12 <suel.sousa12@gmail.com>
2024-11-06 07:57:25 -0300

    Add files via upload

---
+++ Calculadora/src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora;
+
+import Views.Tela_principal;
+
+/**
+ *
+ * @author devmat
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+        Tela_principal tela = new Tela_principal();
+        tela.setVisible(true);
+    }
+    
+    
+    
+    
+}
+
